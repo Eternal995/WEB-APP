a6868022fb97679ee117e9f91ef41f4d767d7c31
Yongda Long <ylong9@myseneca.ca>
2022-09-29 03:08:31 -0400

    Update Assignment 2

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 02
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: Yongda Long
+ *  Student ID: 172800211
+ *  Date: Sept 29, 2022
+ *
+ *  Online (Cyclic) Link: https://fierce-colt-outfit.cyclic.app/
+ *
+ ********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require("path");
