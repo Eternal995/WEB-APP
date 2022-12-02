7e474e281d4c09629c7b6b4d2cc5f313dae21e4b
Yongda Long <ylong9@myseneca.ca>
2022-12-02 02:13:49 -0500

    Update Assignment 6

--- data-service-auth.js
+++ data-service-auth.js
@@ -66,9 +66,7 @@ module.exports.registerUser = (userData) => {
 };
 
 module.exports.checkUser = (userData) => {
-  console.log(userData);
   return new Promise((resolve, reject) => {
-    console.log(userData);
     User.find({ userName: userData.userName })
       .exec()
       .then((user) => {
